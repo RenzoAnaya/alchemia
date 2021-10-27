55c6ebbe1ae45dffbce95198c188205986406317
Renzo Anaya <r.anayaa@alum.up.edu.pe>
2021-10-26 23:09:13 -0500

    página proyecto final

--- js/esquemasimpleproductos.js
+++ js/esquemasimpleproductos.js
@@ -13,24 +13,29 @@ const cafe1 = new Cafe(1,"Zeus","Cuzco",40,"https://cosasbucket.s3.amazonaws.com
 const cafe2 = new Cafe(2,"Hefesto","Villarica",45,"https://4.bp.blogspot.com/-tXCc9h4povY/W9oEORtOUpI/AAAAAAAAMt4/Lf8HbGkdL38IHnniXJgC20zdTjz7JCxUACLcBGAs/s1600/Cafe-Tunki.jpg",15)
 const cafe3 = new Cafe(3,"Ulises","Puno",35,"https://cafesperuanos.com/wp-content/uploads/2020/06/Dwight-Coffeee-grano-entero.png",5)
 const cafe4 = new Cafe(4,"Hércules","Villarica",40,"https://wongfood.vteximg.com.br/arquivos/ids/161157-512-512/Cafe-Molido-Leyenda-Mocca-Bolsa-250-g-479428003.jpg?v=636104988783900000",5)
+const cafe5 = new Cafe(1,"Zeus","Cuzco",40,"https://cosasbucket.s3.amazonaws.com/wp-content/uploads/2020/05/09173832/unnamed-6-300x300.jpg",10)
+const cafe6 = new Cafe(2,"Hefesto","Villarica",45,"https://4.bp.blogspot.com/-tXCc9h4povY/W9oEORtOUpI/AAAAAAAAMt4/Lf8HbGkdL38IHnniXJgC20zdTjz7JCxUACLcBGAs/s1600/Cafe-Tunki.jpg",15)
+const cafe7 = new Cafe(3,"Ulises","Puno",35,"https://cafesperuanos.com/wp-content/uploads/2020/06/Dwight-Coffeee-grano-entero.png",5)
+const cafe8 = new Cafe(4,"Hércules","Villarica",40,"https://wongfood.vteximg.com.br/arquivos/ids/161157-512-512/Cafe-Molido-Leyenda-Mocca-Bolsa-250-g-479428003.jpg?v=636104988783900000",5)
 
-const cafes = [cafe1,cafe2,cafe3,cafe4]
+const cafes = [cafe1,cafe2,cafe3,cafe4,cafe5,cafe6,cafe7,cafe8]
 
 let acumulador = ""
 cafes.forEach(item =>{
     acumulador += `
-                    <div class="card main_card" style="width: 18rem;">
-                        <img class="main__card__img" src="${item.URLimagen}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="main__card__textoprincipal">${item.nombre}</h5>
-                            <p class="main__card__textosecundario">Precio: ${item.precio}</p>
-                            <p class="main__card__textosecundario">Origen: ${item.origen}</p>
-                            <p class="main__card__textosecundario">Stock: ${item.stock}</p>
-                            <a href="#" class="btn btn-primary main__card__boton">Añadir al carrito</a>
+                    <div class="col card h-100">
+                        <div class="card main_card BgProductos contenedorDeImagenes">
+                            <img class="main__card__img" src="${item.URLimagen}" class="card-img-top" alt="cafe">
+                            <div class="card-body ">
+                                <h5 class="main__card__textoprincipal">${item.nombre}</h5>
+                                <p class="main__card__textosecundario">Precio: ${item.precio}</p>
+                                <p class="main__card__textosecundario">Origen: ${item.origen}</p>
+                                <p class="main__card__textosecundario">Stock: ${item.stock}</p>
+                                <a href="#" class="btn btn-primary main__card__boton">Añadir al carrito</a>
+                            </div>
                         </div>
                     </div>`
 })
 
 document.getElementById("container").innerHTML = acumulador
-
 
--- js/productometodos.js
+++ js/productometodos.js
@@ -13,20 +13,26 @@ const metodo1 = new Metodo(1,"Aeropress","Todas",180,"https://cdn.shopify.com/s/
 const metodo2 = new Metodo(2,"V60","Media-fina",150,"https://sc04.alicdn.com/kf/H3f35fa0b264f41bf96cd461552ad897fb.jpg",15)
 const metodo3 = new Metodo(3,"Kalita","Media-fina",180,"https://ae01.alicdn.com/kf/H9827de1aacdc47638e6f0f95fa867fd7h/Japan-Kalita-Hand-Made-Coffee-Pot-Set-Fan-shaped-Drip-Filter-Coffee-Glass-Pot-Three-Hole.jpg",5)
 const metodo4 = new Metodo(4,"Chemex","Media",250,"https://falabella.scene7.com/is/image/FalabellaPE/770682279_1?wid=800&hei=800&qlt=70",5)
+const metodo5 = new Metodo(1,"Aeropress","Todas",180,"https://cdn.shopify.com/s/files/1/0286/1308/3188/products/MetodosNuevoRatio-15-19_590x.png?v=1589677656",3)
+const metodo6 = new Metodo(2,"V60","Media-fina",150,"https://sc04.alicdn.com/kf/H3f35fa0b264f41bf96cd461552ad897fb.jpg",15)
+const metodo7 = new Metodo(3,"Kalita","Media-fina",180,"https://ae01.alicdn.com/kf/H9827de1aacdc47638e6f0f95fa867fd7h/Japan-Kalita-Hand-Made-Coffee-Pot-Set-Fan-shaped-Drip-Filter-Coffee-Glass-Pot-Three-Hole.jpg",5)
+const metodo8 = new Metodo(4,"Chemex","Media",250,"https://falabella.scene7.com/is/image/FalabellaPE/770682279_1?wid=800&hei=800&qlt=70",5)
 
-const metodos = [metodo1,metodo2,metodo3,metodo4]
+const metodos = [metodo1,metodo2,metodo3,metodo4,metodo5,metodo6,metodo7,metodo8]
 
 let acumulador = ""
 metodos.forEach(item =>{
     acumulador += `
-                    <div class="card main__card" style="width: 18rem;">
-                        <img class="main__card__img" src="${item.URLimagen}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="main__card__textoprincipal">${item.nombre}</h5>
-                            <p class="main__card__textosecundario">Precio: ${item.precio}</p>
-                            <p class="main__card__textosecundario">Molienda usada: ${item.molienda}</p>
-                            <p class="main__card__textosecundario">Stock: ${item.stock}</p>
-                            <a href="#" class="btn btn-primary main__card__boton">Añadir al carrito</a>
+                    <div class="col card h-100">
+                        <div class="card main__card BgProductos contenedorDeImagenes">
+                            <img class="main__card__img" src="${item.URLimagen}" class="card-img-top" alt="metodo para preparar cafe">
+                            <div class="card-body contenedorDeImagenes">
+                                <h5 class="main__card__textoprincipal">${item.nombre}</h5>
+                                <p class="main__card__textosecundario">Precio: ${item.precio}</p>
+                                <p class="main__card__textosecundario">Molienda: ${item.molienda}</p>
+                                <p class="main__card__textosecundario">Stock: ${item.stock}</p>
+                                <a href="#" class="btn btn-primary main__card__boton">Añadir al carrito</a>
+                            </div>
                         </div>
                     </div>`
 })
